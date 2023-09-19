544c8606f6abeca7fc23923c5de73b9dfcc91896
Rafael Turse <rafael@brainyyack.com>
2023-09-18 21:09:43 -0400

    js done

--- js/ticket.js
+++ js/ticket.js
@@ -1,11 +1,4 @@
 /**
- * For test purposes, print to console all values from an array
- * 
- * @param {*} array : generic array
- */
-function printArrayValues(array) { for (let i of array){ console.log(i) } }
-
-/**
  * Convert numbers into currency 
  * 
  * @param {*} number 
@@ -19,76 +12,6 @@ function toCurrency(number) { return "$" + (Math.round(number * 100) / 100).toFi
  */
 function ticketReportError() {
     document.getElementById("ticketReport").innerHTML = `<p style="color: red">Verify Given Information</p>` 
-}
-
-/**
- * Assembly a of all js artists literals objects 
- * 
- * @returns : a list of js artists literals objects
- */
-function getArtists() {
-    let artists = []
-
-    artists.push(artistTheBeatles)
-    artists.push(artistBlackSabbath)
-    artists.push(artistOzzyOsbourne)
-
-    return artists
-}
-
-/**
- * Assembly line items for a list
- * 
- * @param {*} array : values
- * @returns values surrounded by li tags
- */
-function getLi(array) {
-    let result = ""
-    
-    for (let i of array) { 
-        result += `<li>`
-        result += `${i}`
-        result += `</li>`
-    }
-
-    return result
-}
-
-/**
- * Assembly artists data in HTML format 
- * 
- * @param {*} artists : artists literals objects
- * @returns : artists html 
- */
-function artistsToHTML (artists) {
-    let htmlArtists = ''
-
-    for (let i of artists) { 
-        htmlArtists += `<div>`
-        htmlArtists += `<p>${i.name}</p>`
-        htmlArtists += `<img src="${i.photo}">`
-        htmlArtists += `<div><ul>${getLi(i.performingTimes)}</ul></div>`
-        htmlArtists += `<div><ul>${getLi(i.musicGenre)}</ul></div>`
-        htmlArtists += `<div><ul>${getLi(i.popularSongs)}</ul></div>`
-        htmlArtists += `</div>`
-    }
-
-    return htmlArtists
-}
-
-/**
- * An HTML section referred as "artists" is required to be replace by artists data
- * 
- */
-function getHtmlArtists() {
-    let artistSection = document.getElementById("artists")
-    let artists = getArtists()
-   
-    /* if artists section not exist, so exit */
-    if (!artistSection) return ""
-
-    /* write artist info into html page */
-    artistSection.innerHTML = artistsToHTML(artists)
 }
 
 /**
